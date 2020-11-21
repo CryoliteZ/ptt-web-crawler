8c69b560e4846ac8bc16df5e85010aacb0f36d78
Alvin Hou <alvinhou@stanford.edu>
2020-11-21 02:49:10 -0800

    Include newlines when crawling ptt

--- PttWebCrawler/crawler.py
+++ PttWebCrawler/crawler.py
@@ -27,6 +27,7 @@ class PttWebCrawler(object):
     PTT_URL = 'https://www.ptt.cc'
 
     """docstring for PttWebCrawler"""
+
     def __init__(self, cmdline=None, as_lib=False):
         parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='''
             A crawler for the web version of PTT, the largest online community in Taiwan.
@@ -57,36 +58,36 @@ class PttWebCrawler(object):
                 self.parse_article(article_id, board)
 
     def parse_articles(self, start, end, board, path='.', timeout=3):
-            filename = board + '-' + str(start) + '-' + str(end) + '.json'
-            filename = os.path.join(path, filename)
-            self.store(filename, u'{"articles": [', 'w')
-            for i in range(end-start+1):
-                index = start + i
-                print('Processing index:', str(index))
-                resp = requests.get(
-                    url = self.PTT_URL + '/bbs/' + board + '/index' + str(index) + '.html',
-                    cookies={'over18': '1'}, verify=VERIFY, timeout=timeout
-                )
-                if resp.status_code != 200:
-                    print('invalid url:', resp.url)
-                    continue
-                soup = BeautifulSoup(resp.text, 'html.parser')
-                divs = soup.find_all("div", "r-ent")
-                for div in divs:
-                    try:
-                        # ex. link would be <a href="/bbs/PublicServan/M.1127742013.A.240.html">Re: [問題] 職等</a>
-                        href = div.find('a')['href']
-                        link = self.PTT_URL + href
-                        article_id = re.sub('\.html', '', href.split('/')[-1])
-                        if div == divs[-1] and i == end-start:  # last div of last page
-                            self.store(filename, self.parse(link, article_id, board), 'a')
-                        else:
-                            self.store(filename, self.parse(link, article_id, board) + ',\n', 'a')
-                    except:
-                        pass
-                time.sleep(0.1)
-            self.store(filename, u']}', 'a')
-            return filename
+        filename = board + '-' + str(start) + '-' + str(end) + '.json'
+        filename = os.path.join(path, filename)
+        self.store(filename, u'{"articles": [', 'w')
+        for i in range(end - start + 1):
+            index = start + i
+            print('Processing index:', str(index))
+            resp = requests.get(
+                url=self.PTT_URL + '/bbs/' + board + '/index' + str(index) + '.html',
+                cookies={'over18': '1'}, verify=VERIFY, timeout=timeout
+            )
+            if resp.status_code != 200:
+                print('invalid url:', resp.url)
+                continue
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            divs = soup.find_all("div", "r-ent")
+            for div in divs:
+                try:
+                    # ex. link would be <a href="/bbs/PublicServan/M.1127742013.A.240.html">Re: [問題] 職等</a>
+                    href = div.find('a')['href']
+                    link = self.PTT_URL + href
+                    article_id = re.sub(r'\.html', '', href.split('/')[-1])
+                    if div == divs[-1] and i == end - start:  # last div of last page
+                        self.store(filename, self.parse(link, article_id, board), 'a')
+                    else:
+                        self.store(filename, self.parse(link, article_id, board) + ',\n', 'a')
+                except BaseException:
+                    pass
+            time.sleep(0.1)
+        self.store(filename, u']}', 'a')
+        return filename
 
     def parse_article(self, article_id, board, path='.'):
         link = self.PTT_URL + '/bbs/' + board + '/' + article_id + '.html'
@@ -126,21 +127,21 @@ class PttWebCrawler(object):
 
         try:
             ip = main_content.find(text=re.compile(u'※ 發信站:'))
-            ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
-        except:
+            ip = re.search(r'[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
+        except BaseException:
             ip = "None"
-
         # 移除 '※ 發信站:' (starts with u'\u203b'), '◆ From:' (starts with u'\u25c6'), 空行及多餘空白
         # 保留英數字, 中文及中文標點, 網址, 部分特殊符號
-        filtered = [ v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--'] ]
-        expr = re.compile(u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
+        filtered = [v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--']]
+        expr = re.compile(u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/\-–_.,?~%()]'))
         for i in range(len(filtered)):
             filtered[i] = re.sub(expr, '', filtered[i])
 
         filtered = [_f for _f in filtered if _f]  # remove empty strings
         filtered = [x for x in filtered if article_id not in x]  # remove last line containing the url of the article
         content = ' '.join(filtered)
-        content = re.sub(r'(\s)+', ' ', content)
+        content = re.sub(r'(\t\r\f\v)+', ' ', content)
+        content = re.sub(r'(\n)+', '\n', content)
         # print 'content', content
 
         # push messages
@@ -155,7 +156,7 @@ class PttWebCrawler(object):
             push_content = push.find('span', 'push-content').strings
             push_content = ' '.join(push_content)[1:].strip(' \t\n\r')  # remove ':'
             push_ipdatetime = push.find('span', 'push-ipdatetime').string.strip(' \t\n\r')
-            messages.append( {'push_tag': push_tag, 'push_userid': push_userid, 'push_content': push_content, 'push_ipdatetime': push_ipdatetime} )
+            messages.append({'push_tag': push_tag, 'push_userid': push_userid, 'push_content': push_content, 'push_ipdatetime': push_ipdatetime})
             if push_tag == u'推':
                 p += 1
             elif push_tag == u'噓':
@@ -188,10 +189,10 @@ class PttWebCrawler(object):
     @staticmethod
     def getLastPage(board, timeout=3):
         content = requests.get(
-            url= 'https://www.ptt.cc/bbs/' + board + '/index.html',
+            url='https://www.ptt.cc/bbs/' + board + '/index.html',
             cookies={'over18': '1'}, timeout=timeout
         ).content.decode('utf-8')
-        first_page = re.search(r'href="/bbs/' + board + '/index(\d+).html">&lsaquo;', content)
+        first_page = re.search(r'href="/bbs/' + board + r'/index(\d+).html">&lsaquo;', content)
         if first_page is None:
             return 1
         return int(first_page.group(1)) + 1
@@ -205,6 +206,7 @@ class PttWebCrawler(object):
     def get(filename, mode='r'):
         with codecs.open(filename, mode, encoding='utf-8') as f:
             return json.load(f)
+
 
 if __name__ == '__main__':
     c = PttWebCrawler()
